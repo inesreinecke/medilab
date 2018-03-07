cf81b5d1d41d67ec62e896f3dde67d202bca88d7
Ines Reinecke <WaBommel@wabommel.speedport_w_724v_09011603_05_013>
2018-03-07 08:56:32 +0100

    added a sidebar alternative and another test view

---
+++ medilab/src/components/Ines.vue
@@ -0,0 +1,37 @@
+<template>
+ <div id="app">
+    <heading></heading>
+    <main class="appMain">
+        <section class="viewCard">
+            <div class="viewCardBody">
+                <h2> Welcome to Ines component</h2>
+                <hr class="breaker">
+                <b-btn variant="primary" v-on:click="send('Ines')">Send Ines</b-btn>
+                <b-btn variant="primary" v-on:click="send('Thomas')">Send Thomas</b-btn>
+            </div>
+        </section>
+        <mySidebar></mySidebar>
+    </main>
+  </div>   
+</template>
+<script>
+import Heading from '@/components/Heading'
+import MySidebar from '@/components/MySidebar'
+
+export default {
+  name: 'ines',
+  components: {
+    Heading,
+    MySidebar
+  },
+  methods: {
+    send: function (name) {
+      // console.log(name)
+      this.$eventHub.$emit('displayButtonContext', {'name': name})
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ medilab/src/components/MySidebar.vue
@@ -0,0 +1,81 @@
+<template>
+ <div class="side">
+     <h1>Hallo</h1>
+     <!-- Ines was selected -->
+    <div v-if="buttonContext=='Ines'">
+        <p class="heading">INES</p>
+        Hallo Ines
+    </div>
+    <!-- Thomas was selected -->
+    <div v-if="buttonContext=='Thomas'">
+         <p class="heading">INES</p>
+        Hallo Thomas
+    </div>
+  </div>   
+</template>
+<script>
+export default {
+  name: 'mySidebar',
+  data () {
+    return {
+      buttonContext: 'none'
+    }
+  },
+  components: {
+  },
+  created () {
+    console.log('initializing MySidebar')
+    // Listen for the i-got-clicked event and its payload.
+    this.$eventHub.$on('displayButtonContext', payload => {
+      console.log(payload)
+      this.buttonContext = payload.name
+    })
+  }
+}
+</script>
+<style scoped>
+
+.side {
+    padding-left: 10px;
+    padding-right: 10px;
+    top: 0;
+    width: 400px;
+    background-color: rgb(213, 216, 219);
+    right: 0;
+    z-index: 10;
+    border-left: 0 solid #fff;
+}
+
+.ownLabel {
+  position: absolute;
+  margin-top: 8px;
+}
+
+.heading {
+  background: #414c55;
+  padding: 10px;
+  margin: 0px -10px 10px -10px;
+  color: #fff;
+}
+
+.breaker {
+  margin-left: -10px;
+  margin-right: -10px;
+  color: #414c55
+}
+
+select option:disabled {
+    color: #000;
+    font-weight: bold;
+}
+
+
+/** disable frame around inputs and textarea in chrome **/
+textarea:focus, input:focus{
+  outline-style:none;
+  box-shadow:none;
+  border-color:transparent;
+}
+
+
+</style>
